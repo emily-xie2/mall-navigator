6b3cc0d1bf67dc38b4f5bf746989959af2b466b3
Emily Xie <107365840+emily-xie2@users.noreply.github.com>
2023-06-22 11:42:52 -0400

    Add files via upload

---
+++ Utility.java
@@ -0,0 +1,23 @@
+/** [Utility.java]
+  * Desc: The class for the utility elements
+  * @author Emily Xie
+  * @version Oct 2022
+  */
+
+public final class Utility{
+    // Declare constants
+    public static final int MOVE = 1;
+    public static final String START = "S";
+    public static final String END = "E";
+    public static final String VISIT = "K";
+    public static final String WALL = " ";
+    public static final String ALLEY = "O";
+    public static final String NO_PATH = "x";
+    
+    /**
+     * Utility
+     * This constructor creates a new Utility object.
+     */
+    private Utility(){ 
+    }
+}
